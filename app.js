a0287ebc22a5b1da76d12e73a9ae9e727b07687c
Ivan <ivan89x@hotmail.com>
2023-12-14 10:30:04 -0300

    db correct constring user

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true } ));
 
 app.use(session({
     store: new pgSession({
-        conString: `postgres://postgres:${process.env.PG_PW}@${process.env.HOSTNAME}/${process.env.PG_DB}`
+        conString: `postgres://imanastronaut:${process.env.PG_PW}@${process.env.HOSTNAME}/${process.env.PG_DB}`
     }),
     secret: process.env.SESSION_SEC,
     resave: false,
